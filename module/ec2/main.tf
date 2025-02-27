755cdf01ff09dd29cb64897000b98e5345385739
Aj <amirthvi@gmail.com>
2025-02-27 19:38:53 +0530

    Vpc configured

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -1,12 +1,53 @@
-resource "aws_instance" "db_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = ""
+
+resource "aws_vpc" "db_server_vpc" {
+  cidr_block         = "13.0.0.0/28"
+  enable_dns_support = true
+}
+
+resource "aws_subnet" "db_server_subnet" {
+  vpc_id     = aws_vpc.db_server_vpc.id
+  cidr_block = "13.0.0.0/28"
 
   tags = {
     Name = "dbserver"
   }
 }
-resource "aws_vpc" "db_server_vpc" {
-  cidr_block = "13.0.0.0/28"
+resource "aws_internet_gateway" "db_igw" {
+  vpc_id = aws_vpc.db_server_vpc.id
+
+  tags = {
+    Name = "dbserver"
+  }
+}
+resource "aws_route_table" "dbroute" {
+  vpc_id = aws_vpc.db_server_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.db_igw.id
+  }
+  tags = {
+    Name = "dbserver"
+  }
+}
+resource "aws_route_table_association" "db_server_association" {
+  subnet_id      = aws_subnet.db_server_subnet.id
+  route_table_id = aws_route_table.dbroute.id
+}
+resource "aws_security_group" "allow_tls" {
+  name        = "db-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.db_server_vpc.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "db_allow_tls"
+  }
 }
--- module/ec2/variable.tf
+++ module/ec2/variable.tf
@@ -1,15 +1,15 @@
 variable "instance_type" {
-  type    = "String"
+  type    = string
   default = "t2.micro"
 
 }
 variable "region_name" {
-  type    = "String"
+  type    = string
   default = "us-east-1"
 
 }
 variable "ami_id" {
-  type    = "String"
+  type    = string
   default = "ami-04b4f1a9cf54c11d0"
 
 }
